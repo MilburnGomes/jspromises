575e6131fc5dd9d6159232e50bed4a8e57cd7a75
MilburnGomes <milburn.gomes@gmail.com>
2020-01-15 01:13:51 +0000

    Added Async / Await / Fetch

--- app.js
+++ app.js
@@ -103,13 +103,13 @@ let cleanRoom = () => {
 };
 
 let removeGarbage = (message) => {
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         resolve(message + ' remove garbage');
     });
 };
 
 let winIcecream = (message) => {
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         resolve(message + ' win Icecream');
     });
 };
@@ -125,3 +125,90 @@ cleanRoom().then(result => {
 });
 
 ///////////////////////////
+
+const posts = [
+
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'This is post two' }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+getPosts();
+
+function createPosts(post) {
+    setTimeout(() => {
+        posts.push(post);
+    }, 2000);
+}
+
+createPosts({ title: 'Post Three', body: 'This is post three' })
+
+function createPostsCallback(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+createPostsCallback({ title: 'Post Four', body: 'This is post four' }, getPosts);
+
+
+function createPostsPromise(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+
+            const error = false;
+
+            if (!error) {
+                resolve();
+            } else {
+                reject('Error: Something went wrong');
+            }
+        }, 2000);
+    });
+}
+
+createPostsPromise({ title: 'Post Five', body: 'This is post five' })
+.then(getPosts)
+.catch(err => {
+    console.log(err);
+});
+
+
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, 'Goodbye');
+});
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+Promise.all([promise1, promise2, promise3]).then(values => console.log(values));
+
+// Async / Await
+async function init() {
+    await createPostsPromise({ title: 'Post Six', bosy: 'This is post six' })
+    getPosts();
+}
+
+init();
+
+// Async / Await / Fetch
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    console.log(data);
+}
+
+fetchUsers();
